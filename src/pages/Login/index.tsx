11dde50c6d124034ff6279a2054dadd63f5da93d
BismarqueSantos <bismarquesilva25@gmail.com>
2022-08-25 17:06:22 -0300

    Retirando modal

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { Button, Modal } from "components";
+import { Button } from "components";
 import { useAuth } from "contexts/Auth";
 
 const PageLogin = () => {
@@ -12,9 +12,11 @@ const PageLogin = () => {
       Boa sorte, e divirta-se no processo, isso é uma experiência. aproveite ao máximo.
       <Button label="Entrar" onClick={handleLogin} />
       <Button label="Cadastrar" onClick={handleLogin} />
-      <Modal>Modal</Modal>
     </>
   );
 };
 
 export default PageLogin;
+
+git config --global user.email "bismarquesilva25@gmail.com"
+  git config --global user.name "BismarqueSantos"
